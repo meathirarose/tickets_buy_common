3fbb61ab542df25a83b095fd2672640d54a07b21
meathirarose <athirarosejohn@gmail.com>
2024-11-16 18:57:19 +0530

    chore: update configurations

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -24,7 +24,9 @@ export const currentUser = (
     }
 
     try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
+        const payload = jwt.verify(
+            req.session.jwt, 
+            process.env.JWT_KEY!) as UserPayload;
         req.currentUser = payload;
     } catch (error) {}
     next();
